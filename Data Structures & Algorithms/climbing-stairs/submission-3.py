87554ec4520e13231e89c56bb79a3555abc510f6
Shreya Gundam <106888384+srgundam1@users.noreply.github.com>
2026-06-27 14:07:59 -0400

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # dp
+        # solve each subproblem once = O(n)
+        # subproblems
+
+
+        if (n<=2):
+            return n
+        
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+
+ 
+        
